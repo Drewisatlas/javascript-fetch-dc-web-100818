403dc0156b5f33f7f199f39d18b814bbf54e6516
Drewisatlas <drewisatlas@gmail.com>
2018-11-26 13:12:57 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 const app = "I don't do much.";
+
+
+let xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://api.github.com/repos/jquery/jquery/commits');
+xhr.responseType = 'json';
+
+xhr.onload = function() {
+  console.log(xhr.response);
+};
+
+xhr.onerror = function() {
+  console.log('Booo');
+};
+
+xhr.send();
+
+// so fetch
+
+fetch('https://api.github.com/repos/jquery/jquery/commits')
+  .then(res => res.json())
+  .then(json => console.log(json));
+
+const token = 'e4aaff25683ab7b57f893bcae49e21b2186b4f4a';
+
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).
+  then(res => res.json()).
+  then(json => console.log(json));
